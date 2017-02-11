f7b83245442ea6f238f0f99d78e602354546eb34
Matt Saladna <matt@apisnetworks.com>
2017-02-10 21:31:42 -0600

    make beacon phar self-executable

--- buildphar.php
+++ buildphar.php
@@ -3,5 +3,6 @@ $phar = new Phar(__DIR__ . '/../beacon.phar', 0, 'beacon.phar');
 // add all files in the project
 $phar->buildFromDirectory(dirname(__FILE__), '!(src|vendor)/.*\.php$!');
 $phar->addFile('beacon.php');
-$phar->setStub($phar->createDefaultStub('beacon.php'));
+$phar->setStub('#!/usr/bin/env php ' ."\n" .
+	"<?php Phar::mapPhar('beacon.phar'); require 'phar://beacon.phar/beacon.php'; __HALT_COMPILER();");
 ?>

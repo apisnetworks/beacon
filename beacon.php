384757cdc30ca8ae7872eb9b2445b6d0e1f82c89
msaladna <matt@apisnetworks.com>
2018-03-05 16:52:04 -0500

    ensure default socket timeout is at least 5 minutes for long-running tasks (e.g. mysql_import)

--- beacon.php
+++ beacon.php
@@ -4,6 +4,8 @@ require __DIR__.'/vendor/autoload.php';
 
 use apisnetworks\Beacon\Commands;
 use Symfony\Component\Console\Application;
+
+ini_set('default_socket_timeout', max((int)ini_get('default_socket_timeout'), 300));
 Helpers::prepStorage();
 $application = new Application('Beacon');
 $application->add(new Commands\EvalCommand());
